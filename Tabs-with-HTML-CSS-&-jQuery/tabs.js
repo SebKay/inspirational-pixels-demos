58e5dd90eb05da75833361c0fa1892585976363c
Seb Kay <personal.sebkay@gmail.com>
2014-01-18 23:13:06 +0000

    Added more examples to tabs demo

--- Tabs-with-HTML-CSS-&-jQuery/tabs.js
+++ Tabs-with-HTML-CSS-&-jQuery/tabs.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	$('.tab-links a').on('click', function(e)  {
+	// Standard
+	$('.tabs.standard .tab-links a').on('click', function(e)  {
 		var currentAttrValue = $(this).attr('href');
 
 		// Show/Hide Tabs
@@ -10,4 +11,44 @@ $(document).ready(function() {
 
 		e.preventDefault();
 	});
-})
+
+	// Animated Fade
+	$('.tabs.animated-fade .tab-links a').on('click', function(e)  {
+		var currentAttrValue = $(this).attr('href');
+
+		// Show/Hide Tabs
+		$('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
+
+		// Change/remove current tab to active
+		$(this).parent('li').addClass('active').siblings().removeClass('active');
+
+		e.preventDefault();
+	});
+
+	// Animated Slide 1
+	$('.tabs.animated-slide-1 .tab-links a').on('click', function(e)  {
+		var currentAttrValue = $(this).attr('href');
+
+		// Show/Hide Tabs
+		$('.tabs ' + currentAttrValue).siblings().slideUp(400);
+		$('.tabs ' + currentAttrValue).delay(400).slideDown(400);
+
+		// Change/remove current tab to active
+		$(this).parent('li').addClass('active').siblings().removeClass('active');
+
+		e.preventDefault();
+	});
+
+	// Animated Slide 2
+	$('.tabs.animated-slide-2 .tab-links a').on('click', function(e)  {
+		var currentAttrValue = $(this).attr('href');
+
+		// Show/Hide Tabs
+		$('.tabs ' + currentAttrValue).slideDown(400).siblings().slideUp(400);
+
+		// Change/remove current tab to active
+		$(this).parent('li').addClass('active').siblings().removeClass('active');
+
+		e.preventDefault();
+	});
+});
