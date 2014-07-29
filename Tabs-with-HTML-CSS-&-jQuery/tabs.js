a7b3c81631895db1b1c98e8bbe883ad403ad2f3c
Seb Kay <personal.sebkay@gmail.com>
2014-07-29 09:21:54 +0100

    Updated demo to work with jQuery 1.11.1

--- Tabs-with-HTML-CSS-&-jQuery/tabs.js
+++ Tabs-with-HTML-CSS-&-jQuery/tabs.js
@@ -1,53 +1,53 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
 	// Standard
-	$('.tabs.standard .tab-links a').on('click', function(e)  {
-		var currentAttrValue = $(this).attr('href');
+	jQuery('.tabs.standard .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
 
 		// Show/Hide Tabs
-		$('.tabs ' + currentAttrValue).show().siblings().hide();
+		jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
 
 		// Change/remove current tab to active
-		$(this).parent('li').addClass('active').siblings().removeClass('active');
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
 
 		e.preventDefault();
 	});
 
 	// Animated Fade
-	$('.tabs.animated-fade .tab-links a').on('click', function(e)  {
-		var currentAttrValue = $(this).attr('href');
+	jQuery('.tabs.animated-fade .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
 
 		// Show/Hide Tabs
-		$('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
+		jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
 
 		// Change/remove current tab to active
-		$(this).parent('li').addClass('active').siblings().removeClass('active');
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
 
 		e.preventDefault();
 	});
 
 	// Animated Slide 1
-	$('.tabs.animated-slide-1 .tab-links a').on('click', function(e)  {
-		var currentAttrValue = $(this).attr('href');
+	jQuery('.tabs.animated-slide-1 .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
 
 		// Show/Hide Tabs
-		$('.tabs ' + currentAttrValue).siblings().slideUp(400);
-		$('.tabs ' + currentAttrValue).delay(400).slideDown(400);
+		jQuery('.tabs ' + currentAttrValue).siblings().slideUp(400);
+		jQuery('.tabs ' + currentAttrValue).delay(400).slideDown(400);
 
 		// Change/remove current tab to active
-		$(this).parent('li').addClass('active').siblings().removeClass('active');
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
 
 		e.preventDefault();
 	});
 
 	// Animated Slide 2
-	$('.tabs.animated-slide-2 .tab-links a').on('click', function(e)  {
-		var currentAttrValue = $(this).attr('href');
+	jQuery('.tabs.animated-slide-2 .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
 
 		// Show/Hide Tabs
-		$('.tabs ' + currentAttrValue).slideDown(400).siblings().slideUp(400);
+		jQuery('.tabs ' + currentAttrValue).slideDown(400).siblings().slideUp(400);
 
 		// Change/remove current tab to active
-		$(this).parent('li').addClass('active').siblings().removeClass('active');
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
 
 		e.preventDefault();
 	});
