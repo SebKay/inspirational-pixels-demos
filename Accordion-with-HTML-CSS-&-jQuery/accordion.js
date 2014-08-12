2bf7fb8457b33947de7911e564e8cf45a554f678
Seb Kay <personal.sebkay@gmail.com>
2014-08-12 12:52:03 +0100

    Updated accordion demo to new style

--- Accordion-with-HTML-CSS-&-jQuery/accordion.js
+++ Accordion-with-HTML-CSS-&-jQuery/accordion.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
 	function close_accordion_section() {
-		$('.accordion .accordion-section-title').removeClass('active');
-		$('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+		jQuery('.accordion .accordion-section-title').removeClass('active');
+		jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
 	}
 
-	$('.accordion-section-title').click(function(e) {
+	jQuery('.accordion-section-title').click(function(e) {
 		// Grab current anchor value
-		var currentAttrValue = $(this).attr('href');
+		var currentAttrValue = jQuery(this).attr('href');
 
-		if($(e.target).is('.active')) {
+		if(jQuery(e.target).is('.active')) {
 			close_accordion_section();
 		}else {
 			close_accordion_section();
 
 			// Add active class to section title
-			$(this).addClass('active');
+			jQuery(this).addClass('active');
 			// Open up the hidden content panel
-			$('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+			jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
 		}
 
 		e.preventDefault();
