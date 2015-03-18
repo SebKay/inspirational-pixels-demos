a2a2f86a72560df4a6d10a7ec07ec7c369d6a435
Seb Kay <personal.sebkay@gmail.com>
2015-03-18 15:57:43 +0000

    jQuery for opening the popup

--- popup-modal/jquery.popup.js
+++ popup-modal/jquery.popup.js
@@ -1,3 +1,9 @@
 $(function() {
-	
+	$('[data-popup-open]').on('click', function(e)  {
+		var targeted_popup_class = jQuery(this).attr('data-popup-open');
+
+		$('[data-popup="' + targeted_popup_class + '"]').addClass('popup-open');
+
+		e.preventDefault();
+	});
 });
